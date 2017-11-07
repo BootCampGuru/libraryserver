ef2cc3d615793b13db33a7f9fb4a88464a50bfac
rbiswas <rbiswas@crs.loc.gov>
2017-11-07 16:58:37 -0500

    'server'

---
+++ Auth.js
@@ -0,0 +1,38 @@
+module.exports = {
+	
+	tone_analyzer:{
+   	"url": "https://gateway.watsonplatform.net/tone-analyzer/api",
+     "username": "**",
+     "password": "**",
+  	  version:"v3",
+	  "version_date" : "2016-05-19"
+	},
+	language_translation:{
+	 "url": "https://gateway.watsonplatform.net/language-translator/api",
+     "username": "**",
+     "password": "**",
+	  version:"v2"
+	},
+	text_to_speech:{
+	  "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize",
+      "username": "**",
+      "password": "**",
+      version:"v1"
+	},
+	speech_to_text:{
+	  "url": "https://stream.watsonplatform.net/speech-to-text/api",
+      "username": "**",
+      "password": "**",
+      version:"v1"
+	},
+	watson:{
+		conversation:{
+     "url": "https://gateway.watsonplatform.net/conversation/api",
+     "username": "**",
+    "password": "**",
+  	 version:"v1",
+	 "version_date" : "2017-05-26",
+	  workspace_id: "**"
+	}
+}
+}
