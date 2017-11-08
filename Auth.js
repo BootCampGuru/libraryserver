0924367225aa4213716611b70bd4502aded47a6f
Ranjan Biswas <30506711+BootCampGuru@users.noreply.github.com>
2017-11-08 13:05:06 -0500

    Remove passwords

--- Auth.js
+++ Auth.js
@@ -28,8 +28,8 @@ module.exports = {
 	watson:{
 		conversation:{
       "url": "https://gateway.watsonplatform.net/conversation/api",
-      "username": "ce57718f-d88d-40e7-92cb-cf854915f9f4",
-      "password": "lP3Bh4WtT1YG",
+      "username": "**",
+      "password": "**",
   	   version:"v1",
 	  "version_date" : "2017-05-26",
 	   workspace_id: "3784c0c9-714f-40d9-821c-7e98587b8335"
